08bc5e53e78221f97550da3e88cc170e0301b3cf
James O'Neal <jamesponeal@gmail.com>
2016-04-26 10:37:47 -0500

    adds lightbox files

---
+++ jQuery-lightbox/js/app.js
@@ -0,0 +1,13 @@
+//Problem: User when clicking on image goes to a dead end.
+//Solution: Create an overlay with the large image - lightbox.
+
+//1. Capture the click event on a link to an image.
+$().click
+  //1.1 Show the overlay.
+  //1.2 Update overlay with the image linked in the link.
+  //1.3 Add the 'alt' attribute (caption).
+//2. Add overlay.
+  //2.1 An image.
+  //2.2 A caption.
+//3. When overlay is clicked.
+  //3.1 Hide overlay.
