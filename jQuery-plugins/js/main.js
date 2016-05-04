892be857b3d2327ee9edde34db92e21fa72fb50e
James O'Neal <jamesponeal@gmail.com>
2016-05-04 16:18:31 -0500

    finishes sticky nav bar exercise

--- jQuery-plugins/js/main.js
+++ jQuery-plugins/js/main.js
@@ -5,3 +5,31 @@ $(".animsition").animsition({
   inDuration: 1000,
   outDuration: 500
 });
+
+$(".header").sticky({
+  topSpacing:0,
+  getWidthFrom: '.container',
+  responsiveWidth: true
+});
+
+$(".header").on('sticky-start', function(){
+  $(".description").html('We build <strong>great</strong> apps');
+});
+
+$(".header").on('sticky-end', function(){
+  $(".description").html('We build apps');
+});
+
+$(".want-us").sticky({
+  topSpacing:64,
+  getWidthFrom: '.container',
+  responsiveWidth: true
+});
+
+$(".want-us").on('sticky-start', function(){
+  $(".want-us").html('Want us to work on your project? <a href="#">Email Us</a>');
+});
+
+$(".want-us").on('sticky-end', function(){
+  $(".want-us").html('Want us to work on your project?');
+});
