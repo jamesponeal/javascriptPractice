dabcb5ec5ebe0aa7252a1bd18b6dcabf9fcfa2e1
James O'Neal <jamesponeal@gmail.com>
2016-06-06 19:52:47 -0600

    adds functional employee callback

--- AJAX-JSON/js/widget.js
+++ AJAX-JSON/js/widget.js
@@ -3,8 +3,27 @@ xhr.onreadystatechange = function(){
   if(xhr.readyState === 4){
     //console.log(typeof xhr.responseText);
     var employees = JSON.parse(xhr.responseText);
-    console.log(employees);
+    var statusHTML = '<ul class="bulleted">';
+    for(var i=0; i < employees.length; i += 1){
+      if(employees[i].inoffice === true){
+        statusHTML += '<li class="in">';
+      } else {
+        statusHTML += '<li class="out">';
+      }
+      statusHTML += employees[i].name;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('employeeList').innerHTML = statusHTML;
   }
 };
 xhr.open('GET','info/employees.json');
 xhr.send();
+
+var xhrRoom = new XMLHttpRequest();
+xhrRoom.onreadystatechange = function(){
+  if(xhrRoom.readyState === 4){
+    var rooms = JSON.parse(xhrRoom.responseText);
+    var statusHTML =
+  }
+}
