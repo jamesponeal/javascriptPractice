80cdb3863ba0fe9d1929e265455be0bc60293c24
James O'Neal <jamesponeal@gmail.com>
2016-06-16 13:59:39 -0500

    adds prototype function to Dice constructor function

--- OOJ-dice/dice.js
+++ OOJ-dice/dice.js
@@ -1,7 +1,18 @@
-var dice = {
-  sides : 1000,
-  roll : function(){
-    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    return randomNumber;
-  }
+// var dice = {
+//   sides : 1000,
+//   roll : function(){
+//     var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//     return randomNumber;
+//   }
+// }
+
+function Dice(sides){
+  this.sides = sides;
 }
+
+Dice.prototype.roll = function(){
+  var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+  return randomNumber;
+}
+
+var dice = new Dice(100)
